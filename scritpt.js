9436e0391c351cc6b7c221dd0279085d41172ea2
SRIRAMOJUSRAVANTHI <147321410+SRIRAMOJUSRAVANTHI@users.noreply.github.com>
2023-10-10 21:01:59 +0530

    Add files via upload

---
+++ scritpt.js
@@ -0,0 +1,51 @@
+console.log("hello good evening");
+
+
+let  var1=2;
+let var2=6;
+let var3=12;
+let var4=12;
+let ageOfAPerson=30; //camel case
+let age_of_a_person=60;
+let nameOfAPerson="srav";  //string
+let firstLetterOfAWord='k';   //char
+
+//rules camel case, word in double quotes, no spaces in creating a variable name
+
+//it will not take the updated values it will remain same and gets uncaught type error
+
+
+let ageOfAPerson1=30;
+ageOfAPerson1=40;
+console.log(nameOfAPerson);
+console.log(var2);
+console.log(ageOfAPerson1);
+
+
+console.log(var1+var2);
+ let age=89;
+ age++;
+ let age1=67;
+ age1--;
+ console.log(age);
+ console.log(age1);
+
+ let apple=12;
+ apple +=6;
+ console.log(apple); //*=, +=, -+, /= .....assignment operators
+
+
+ console.log(var1==var2);// comparision operators
+ console.log(var3==var4);
+ console.log(var1!=var2);
+ console.log(var1>=var2);
+ console.log(var3<var2);
+ // datatypes : number
+ //boolean-true,false
+ //string= collection of characters ex="srav","hello","44"...etc
+
+ let num1=55;
+ let num2="55";
+ console.log(num1==num2);//true
+ console.log(num1===num2);// false becoz it will check number and datatype  also
+
